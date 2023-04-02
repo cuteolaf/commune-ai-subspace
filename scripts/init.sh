da5f07ccc7bc62ce8f36d783fe45dc55fb331034
salvivona <vivonasg@gmail.com>
2023-04-02 11:27:02 -0400

    bro

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,12 +1,24 @@
-#!/usr/bin/env bash
-# This script is meant to be run on Unix/Linux based systems
-set -e
+#/bin/bash
 
-echo "*** Initializing WASM build environment"
+# Install cargo and Rust
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+PATH="/root/.cargo/bin:${PATH}"
+source $HOME/.cargo/env
 
-if ! (( ${#CI_PROJECT_NAME} )) ; then
-   rustup update nightly
-   rustup update stable
-fi
+apt-get update \
+ && DEBIAN_FRONTEND=noninteractive \
+    apt-get install --no-install-recommends --assume-yes \
+      protobuf-compiler
 
+rustup update nightly
 rustup target add wasm32-unknown-unknown --toolchain nightly
+apt-get install make
+apt-get install -y pkg-config
+
+# CONTRACTS STUFF
+apt install binaryen
+apt-get install libssl-dev
+cargo install cargo-dylint dylint-link
+cargo install cargo-contract --force
+
+rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
